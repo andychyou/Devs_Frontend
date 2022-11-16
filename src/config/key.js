41c4a7f451a9790fa2f270ffc488bf226c55d822
sen <seeunchoi99@gmail.com>
2022-11-16 22:28:51 +0900

    deploy: change backend url

--- src/config/key.js
+++ src/config/key.js
@@ -3,4 +3,4 @@ import axios from "axios"
 axios.defaults.xsrfCookieName = 'csrftoken'
 axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN'
 
-export const APIURL = 'http://devs-backend-dahyen0o.koyeb.app'
+export const APIURL = 'https://devs-backend-dahyen0o.koyeb.app'
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app){
   app.use(
     createProxyMiddleware(['/api'], {
       // target: "http://127.0.0.1:8000",
-      target: "http://devs-backend-dahyen0o.koyeb.app",
+      target: "https://devs-backend-dahyen0o.koyeb.app",
       changeOrigin: true
     })
   )
