ffa4083b7891d61efed9eb0e7608eda3b5246749
taeho0888 <taeho0888@naver.com>
2022-11-18 14:58:13 +0900

    modify proxy url

--- src/config/key.js
+++ src/config/key.js
@@ -4,4 +4,4 @@ axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 
 // export const APIURL = 'https://devs-backend-dahyen0o.koyeb.app'
-export const APIURL = "http://127.0.0.1:8000";
+export const APIURL = "https://devs-tory.herokuapp.com";
