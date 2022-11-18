eda1b22663e21583db38d22e67db1d669fa1a971
Hanrabong <48653304+andychyou@users.noreply.github.com>
2022-11-18 13:54:00 +0900

    Update SearchResultCard.jsx

--- src/components/search/SearchResultCard.jsx
+++ src/components/search/SearchResultCard.jsx
@@ -19,28 +19,60 @@ const SearchResultCard = ({id}) => {
       console.log("get user info fail");
     }
   };
+  const [profile, setProfile] = useState({})
+  const getProfile = async () => {
+    const res = await axios.get(`${APIURL}/profile/profile/${id}/`)
+    if (res.status == 200) {
+      setProfile(res.data);
+    } else {
+      console.log("get profile fail");
+    }
+  };
+
+  const onFollow = async () => {
+    const follower = getCookie("user_id");
+
+    if (!follower) {
+      alert("로그인이 필요합니다!");
+      return;
+    }
+
+    const res = await axios.post(`${APIURL}/profile/follow/`, {
+      follower,
+      following: userInfo.id,
+    });
+
+    if (res.status == 201) {
+      alert("나의 아이돌에 추가되었습니다.");
+    } else if (res.status == 204) {
+      alert("나의 아이돌에서 삭제되었습니다.");
+    }
+  };
+  
+
   useEffect(()=>{
-  console.log('id',id)
   getUserInfo()
-  },[])
+  getProfile()
+  },[id])
+
   return (
     <SearchResultDiv>
     <SearchResultImg src={userInfo.image}></SearchResultImg>         
       <SearchResultDetail>
           <div style={{fontSize: '30px', fontWeight: '700'}}>{userInfo.name}</div>
           <div>
-            <span style={{fontSize: '24px'}}>Frontend</span>
-            <span style={{fontSize: '24px'}}>@ job</span>
+            <span style={{fontSize: '24px'}}>{profile.main_position}</span>
+            <span style={{fontSize: '24px'}}>@ {profile.belong}</span>
           </div>              
-          <div style={{fontSize: '24px'}}>school</div>
+          <div style={{fontSize: '24px'}}>{profile.major}</div>
       </SearchResultDetail>
       <SearchResultIntroBox>
         <p>
-          axios get intro
+          {profile.introduction}
         </p>
       </SearchResultIntroBox>
       
-      <SearchResultButton>
+      <SearchResultButton onClick={onFollow}>
           IDOL 등록
       </SearchResultButton>
     </SearchResultDiv>
