e8bd56f513f847cb046ef00608a5791a6b53d8d5
taeho0888 <taeho0888@naver.com>
2022-11-21 17:11:20 +0900

    modify proxy url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,8 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/api"], {
-      target: "http://127.0.0.1:8000",
-      // target: "https://devs-tory.herokuapp.com",
+      // target: "http://127.0.0.1:8000",
+      target: "https://devs-tory.herokuapp.com",
       // target: "https://devs-backend-dahyen0o.koyeb.app",
       changeOrigin: true,
     })
